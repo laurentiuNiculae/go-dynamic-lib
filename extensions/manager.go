62a6853ff9a2c2cffe8248d6cc99ca5a280da729
laurentiuNiculae <themelopeus@gmail.com>
2022-04-05 17:34:29 +0300

    wow

--- extensions/manager.go
+++ extensions/manager.go
@@ -1,11 +1,22 @@
 package extensions
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"plugin"
+)
+
+const (
+	pluginsDir = "plugins"
+)
+
 type PluginManager struct {
 	implementations map[string]map[string]interface{}
 	config          Config
 }
 
-var implementations map[string]map[string]interface{}
+var implementations map[string]map[string]interface{} = map[string]map[string]interface{}{}
 
 func NewPluginManager(config Config) PluginManager {
 	return PluginManager{
@@ -15,7 +26,7 @@ func NewPluginManager(config Config) PluginManager {
 }
 
 func (pm *PluginManager) RegisterImplementation(extPoint string, implName string, plugin interface{}) {
-	// do tests
+	// do verifications
 	pm.implementations[extPoint][implName] = plugin
 }
 
@@ -24,5 +35,28 @@ func (pm *PluginManager) Config() Config {
 }
 
 func (pm *PluginManager) LoadPlugins(pluginDir string) {
-	
+	dirs, err := os.ReadDir(pluginsDir)
+	if err != nil {
+		panic("I cry, no pluginDir.")
+	}
+
+	for _, d := range dirs {
+		if !d.IsDir() {
+			continue
+		}
+
+		plugin, err := plugin.Open(filepath.Join(pluginsDir, d.Name(), d.Name()+".so"))
+		if err != nil {
+			fmt.Println("Cryy!!", err)
+			continue
+		}
+
+		f, err := plugin.Lookup("Register")
+		if err != nil {
+			fmt.Println("Cryy!! Can't find register :(")
+			continue
+		}
+		registerFunction := f.(func(*PluginManager))
+		registerFunction(pm)
+	}
 }
--- main.go
+++ main.go
@@ -9,5 +9,4 @@ func main() {
 	})
 
 	pm.LoadPlugins("/plugins")
-
 }
